d279f4386bf958cbd6cc36f697df96434d2673fe
YXLONG <yangxiaolong3@jd.com>
2017-01-22 14:19:16 +0800

    no message

---
+++ XIAlertSDK/XIAlertSDK.h
@@ -0,0 +1,20 @@
+//
+//  XIAlertSDK.h
+//  XIAlertSDK
+//
+//  Created by YXLONG on 2017/1/22.
+//  Copyright © 2017年 yxlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XIAlertSDK.
+FOUNDATION_EXPORT double XIAlertSDKVersionNumber;
+
+//! Project version string for XIAlertSDK.
+FOUNDATION_EXPORT const unsigned char XIAlertSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XIAlertSDK/PublicHeader.h>
+
+
+#import <XIAlertSDK/XIAlertView.h>
